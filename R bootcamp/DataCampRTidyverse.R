9e720843f28be48ccaf4397c63888f745bca1c5a
IvyStreeter <ivystreeter@protonmail.com>
2024-06-01 21:07:16 -0400

    Completed Introduction to the Tidyverse for R

--- R bootcamp/DataCampRTidyverse.R
+++ R bootcamp/DataCampRTidyverse.R
@@ -153,31 +153,131 @@ by_year
 ggplot(by_year, aes(x = year, y = totalPop)) + 
   geom_point()
  
+# Start y-axis at zero
+ggplot(by_year, aes(x = year, y = totalPop)) + 
+  geom_point() + 
+  expand_limits(y = 0)
+
+# Summarize by year and continent
+by_year_continent <- gapminder %>%
+  group_by(year, continent) %>%
+  summarize(totalPop = sum(pop),
+            medianLifeExp= median(lifeExp))
+
+by_year_continent
+
+# Separate by color
+ggplot(by_year_continent, aes(x = year, y = totalPop,
+                              color = continent)) + 
+  geom_point() + 
+  expand_limits(y = 0)
+
+# Plot the change in medianGdpPercap in each continent over time
+ggplot(by_year_continent, aes(x = year, y = medianGdpPercap,
+                              color = continent)) + 
+  geom_point() + 
+  expand_limits(y = 0)
+
+# Summarize the median GDP and median life expectancy per continent in 2007
+by_continent_2007 <- gapminder %>%
+  filter(year == 2007) %>%
+  group_by(continent) %>%
+  summarize(medianLifeExp = median(lifeExp), 
+            medianGdpPercap = median(gdpPercap))
+
+# Use a scatter plot to compare the median GDP and median life expectancy
+ggplot(by_continent_2007, aes(x = medianGdpPercap, y = medianLifeExp,
+                              color = continent)) + 
+  geom_point() 
 
 
+# Scatter plots are useful to compare 2 variables
+# Line plots are useful for change over time
+# Barplots comparing statistics between categorical variables
+# Histograms describe distribution
+# Box plot, describe distribution over categorical variables
+
+# Summarize the median gdpPercap by year, then save it as by_year
+by_year <- gapminder %>%
+  group_by(year) %>%
+  summarize(medianGdpPercap = median(gdpPercap))
+
+# Create a line plot showing the change in medianGdpPercap over time
+ggplot(by_year, aes(x = year, y = medianGdpPercap)) +
+  geom_line() +
+  expand_limits(y = 0)
+
+# Summarize the median gdpPercap by year & continent, save as by_year_continent
+by_year_continent <- gapminder %>%
+  group_by(year, continent) %>%
+  summarize(medianGdpPercap = median(gdpPercap))
+
+# Create a line plot showing the change in medianGdpPercap by continent over time
+ggplot(by_year_continent, aes(x = year, y = medianGdpPercap, color = continent)) +
+  geom_line() +
+  expand_limits(y = 0)
 
 
+# Barplots
+by_continent <- gapminder %>%
+  filter(year == 2007) %>%
+  group_by(continent) %>%
+  summarize(meanLifeExp = mean(lifeExp))
+
+by_continent
+
+ggplot(by_continent, aes(x = continent, y = meanLifeExp)) +
+  geom_col()
+
+# Create a bar plot showing medianGdp by continent
+ggplot(by_continent, aes(x = continent, y = medianGdpPercap)) +
+  geom_col()
+
+# Summarize the median gdpPercap by continent in 1952
+by_continent <- gapminder %>%
+  filter(year == 1952) %>%
+  group_by(continent) %>%
+  summarize(medianGdpPercap = median(gdpPercap))
+
+# Create a bar plot showing medianGdp by continent
+ggplot(by_continent, aes(x = continent, y = medianGdpPercap)) +
+  geom_col()
+
+# Filter for observations in the Oceania continent in 1952
+oceania_1952 <- gapminder %>%
+  filter(continent == "Oceania", year == 1952)
 
 
+# Create a bar plot of gdpPercap by country
+ggplot(oceania_1952, aes(x = country, y = gdpPercap)) +
+  geom_col()
 
+# Histograms
+gapminder_2007 <- gapminder %>%
+  filter(year == 2007)
 
+ggplot(gapminder_2007, aes(x = lifeExp)) +
+  geom_histogram()
 
+# Set bin width to change resolution/length of years per bin
+# Helps with understanding general trends
 
+# you can change the length of each bin
+ggplot(gapminder_2007, aes(x = lifeExp)) +
+  geom_histogram(binwidth = 5)
 
+# you can change the number of bins
+ggplot(gapminder_2007, aes(x = lifeExp)) +
+  geom_histogram(bins = 5)
 
+# Boxplots
+ggplot(gapminder_2007, aes(x = continent, y = lifeExp)) +
+  geom_boxplot()
 
-
-
-
-
-
-
-
-
-
-
-
-
+ggplot(gapminder_2007, aes(x = continent, y = gdpPercap)) +
+  geom_boxplot() +
+  scale_y_log10() +
+  ggtitle("Comparing GDP per capita across continents")
 
 
 
